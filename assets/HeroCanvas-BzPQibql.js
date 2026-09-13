370273a51bd93be17a0769bb27b40a2df688ebdd
vanetiaho <vanetiaho@users.noreply.github.com>
2026-09-13 14:54:36 +0000

    deploy: 8118c57582b3ea3f87a99f5c3f8baa1f5ab8a859

--- assets/HeroCanvas-BzPQibql.js
+++ assets/HeroCanvas-BzPQibql.js
@@ -1,4 +1,4 @@
-import{r as Pe,g as uA,j as Ot,u as hA,b as fA}from"./index-DBK_R4Ox.js";/**
+import{r as Pe,g as uA,j as Ot,u as hA,b as fA}from"./index-CrsZwhDj.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
